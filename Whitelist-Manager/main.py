5b87f129b8ffeca992d9f02fa2326260684d3eb1
rcollette <colletterc@vcu.edu>
2022-08-19 16:34:02 -0400

    Finished whitelist/wallet submit bot basic features.

--- Whitelist-Manager/main.py
+++ Whitelist-Manager/main.py
@@ -1,3 +1,4 @@
+from tkinter import HIDDEN
 import discord, pymongo, requests, aiohttp, time, asyncio, dataclasses
 from discord.ext import commands
 from discord import Webhook, AsyncWebhookAdapter
@@ -7,46 +8,81 @@ from discord_slash import SlashCommand
 client = commands.Bot(command_prefix='?')
 client.remove_command('help')
 slash = SlashCommand(client, sync_commands=True)
-#mongo = pymongo.MongoClient("mongodb+srv://backend_user:Faggoted123@discordwhitelistbot.53exszw.mongodb.net/?retryWrites=true&w=majority")
-#db = mongo.test
+mongo = pymongo.MongoClient("mongodb+srv://backend_user:Faggoted123@discordwhitelistbot.53exszw.mongodb.net/?retryWrites=true&w=majority")
+db = mongo['Server']
+info = db['Wallets']
 
 @client.event
 async def on_ready():
     print('Bot Online')
 
+#Done, working, needs embeds
 @slash.slash(description="Submits user's wallet")
 async def submit(ctx, wallet:str):
-    #if query for ctx.author_id returns wallet -> return discord interaction already submitted with wallet
-    #else submit ctx.author_id as search column and wallet as the value
-    print(f'{ctx.author_id} - wallet -> {wallet}')
+    query = {"id": f"{ctx.author_id}"}
+    do_query = info.find(query, {"_id": 0, "id": 0 })
+    result = list(do_query)
+    try:
+        if len(result)!=0:
+            wallet_return = result[0]['address'].replace("'","")
+            await ctx.send(f"```Wallet is already submtted: {wallet_return}```", hidden=True)
+        else:
+            new_wallet = { "id": f"{ctx.author_id}", "address": f"{wallet}" }
+            info.insert_one(new_wallet)
+            await ctx.send(f"```Wallet: {wallet} submitted for {ctx.author}```", hidden=True)
 
+    except Exception as e:
+        print(e)
+
+#Done, working, needs embed
 @slash.slash(description="Returns the submitted wallet")
 async def check(ctx):
-    #query for ctx.author_id and return wallet
-    #else query returns empty return suggestion to submit their wallet using /submit
-    print(f'{ctx.author_id}')
+    query = {"id": f"{ctx.author_id}"}
+    do_query = info.find(query, {"_id": 0, "id": 0 })
+    result = list(do_query)
+    try:
+        if len(result)==0:
+            await ctx.send("```User has not submitted wallet, please submit using /submit```", hidden=True)
+        else:
+            wallet_return = result[0]['address'].replace("'","")
+            await ctx.send(f"```{wallet_return}```", hidden=True)
 
+    except Exception as e:
+        print(e)
+
+#Done, working, needs embed
 @slash.slash(description="Remove submitted wallet")
-async def delete(ctx, wallet:str):
-    #query for ctx.author_id -> if there is a value for wallet, remove the entry from db -> return success to user
-    #else query does not return -> return to user that they should submit their wallet
-    print(f'{ctx.author_id}')
+async def delete(ctx):
+    query = {"id": f"{ctx.author_id}"}
+    do_query = info.find(query, {"_id": 0, "id": 0 })
+    result = list(do_query)
+    try:
+        if len(result) == 0:
+            await ctx.send(f"```No wallet has been submitted, submit one using /submit```", hidden=True)
+        else:
+            await ctx.send("```Wallet has been removed, you may resubmit using /submit```", hidden=True)
+            info.delete_one(query)
+    except Exception as e:
+        print(e)
 
+#Done, working, needs embed
 @slash.slash(description="[DEV] Returns user's wallet to developer.")
-async def get(ctx, id:int):
-    #query for id parameter -> if user has submitted wallet return the query
-    #else query returns nothing -> return user has not submitted wallet
-    print(f'{ctx.author_id}')
+async def get(ctx, id:str):
+    query = {"id": f"{id}"}
+    do_query = info.find(query, {"_id": 0, "id": 0 })
+    result = list(do_query)
+    try:
+        if len(result) == 0:
+            await ctx.send(f"```User has not submitted a wallet```", hidden=True)
+        else:
+            wallet_return = result[0]['address'].replace("'","")
+            await ctx.send(f"```Wallet for {id}: {wallet_return}```", hidden=True)
+    except Exception as e:
+        print(e)
 
-@slash.slash(description="[ADMIN] Starts giveaway")
-async def startgiveaway(ctx, wallet:str):
-    #add parameters to this
-    #use parameters to form new embed
-    #add reaction for users to react to
-    #when user reacts add them to array
-    #use winners# parameter to pick that amount of people and add them to array, if a user is selected twice, do not add them to array again
-    #refrence giveaway bot source for some of the logic
-    print(f'{ctx.author_id}')
+#Implement this later
+#@slash.slash(description="[ADMIN] Starts giveaway")
+#async def startgiveaway(ctx, wallet:str):
 
 client.run('MTAxMDE5NTUyNjYwNTYxNTIwNA.GRghpd.boPfQY2ZANM9JUe9o4AjfL6oa6lEumStdq0g1E')
 
